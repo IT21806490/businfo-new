1de80a68d8501bf090711db20f86e503208f0851
Niranga Theekshana <142475586+IT21806490@users.noreply.github.com>
2025-09-19 20:43:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-// src/App.jsx
-import FareCalculator from './components/FareCalculator';
-import './index.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import FindRoutes from './components/FindRoutes';
+import Contact from './components/Contact';
+import Fares from './components/Fares';
+import '../src/main';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <FareCalculator />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/routes" element={<FindRoutes />} />
+        <Route path="/fares" element={<Fares />} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
