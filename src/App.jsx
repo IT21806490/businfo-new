7fd984bcc61fc9505be4069ef5a14bbdc22948f0
Niranga Theekshana <142475586+IT21806490@users.noreply.github.com>
2025-09-19 21:23:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import FindRoutes from './components/FindRoutes';
 import Contact from './components/Contact';
@@ -8,14 +8,14 @@ import '../src/main';
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/routes" element={<FindRoutes />} />
         <Route path="/fares" element={<Fares />} />
         <Route path="/contact" element={<Contact/>} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
